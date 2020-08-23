378742b1cc44bc16c61b3a6d6e92dbe032baaf9e
Felipe Jung <felipemattoseu@gmail.com>
2020-08-23 11:48:36 -0400

    updating storybook version

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,3 +1,4 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
 import { withKnobs, text } from '@storybook/addon-knobs';
 
 import Main from '.';
@@ -6,9 +7,9 @@ export default {
   title: 'Main',
   component: Main,
   decorators: [withKnobs]
-};
+} as Meta;
 
-export const Basic = () => (
+export const Basic: Story = () => (
   <Main
     title={text('Title', 'Next.js')}
     description={text(

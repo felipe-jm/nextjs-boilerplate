426e99aa59f5c3dd0ed50386deb274dc9a1dde99
Felipe Jung <felipemattoseu@gmail.com>
2021-09-13 15:23:15 -0400

    chore(deps): update deps da fix storybook next router

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,12 @@
+import { RouterContext } from 'next/dist/shared/lib/router-context';
+
 import GlobalStyles from '../src/styles/global';
 
+export const parameters = {
+  nextRouter: {
+    Provider: RouterContext.Provider
+  }
+};
 export const decorators = [
   (Story) => (
     <>

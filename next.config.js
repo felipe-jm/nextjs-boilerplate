ff36de0939004a2d2e25ea3ba8a134003246fd0c
Felipe Jung <felipemattoseu@gmail.com>
2023-01-17 09:37:03 -0400

    chore(deps): update deps

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa');
+const withPWA = require('next-pwa')({
+  dest: 'public'
+});
 const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = withPWA({
